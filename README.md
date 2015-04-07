a5cdf072e5cb05e548cfee2b4e4aabeb1e9f68d6
Marlus Araujo <m@marlus.com>
2015-04-07 17:11:02 -0300

    carregamento dos dados

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# p22-cartaz-generativo
+# P22 Cartaz Generativo
+
+Cada edição das Olimpíadas gera uma quantidade imensa de dados: localização, horas, recordes, quadro de medalhas. O algorítmo carrega regras e conceitos criados pelos designers, e com os dados de cada edição, gera visualizações para um cartaz que representa aquele ano.
+
+<http://iedrioppd.github.io/p22-cartaz-generativo/>
+
+---
+
+Para rodar localmente, crie um servidor python para permitir os requests ajax
+
+    python -m SimpleHTTPServer 8000
+
+e acesse <http://localhost:8000/>
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,56 @@
-console.log('Hello, console!')
+var APP,
+	DATA = []
+
+// PROCESSAMENTO DE DADOS
+
+function loadCSV(file,id,callback){
+
+    // DVS para carregar CSV separado por ;
+
+    var dsv = d3.dsv(',', 'text/plain')
+
+    dsv(file[1])
+        .row(function(d){
+            return d
+        })
+        .get(function(error, rows){
+            
+            DATA[file[0]] = rows
+            
+            if(callback){
+                callback(id)
+            }
+        });
+}
+
+function loadDataset(arr,callback) {
+    var count = 0
+    var id = 0
+    var cbk = function(id){
+        count++
+        if(count < arr.length){
+            loadCSV(arr[count], id, cbk)
+        } else {
+            if(callback){
+                callback()
+            }
+        }
+    };
+    loadCSV(arr[0],id,cbk)
+}
+
+// APP
+
+APP = {
+	init: function(){
+		console.log('Hello, d3!')
+		console.log(DATA['countries'])
+	}
+}
+
+// CARREGA DATASET E INICIA
+
+loadDataset([
+    ['countries','data/countries.csv'],
+    ['olympics','data/summer-olympic-medallists-1896-2008.csv']
+], APP.init);
